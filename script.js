fd0f763c9e44352c3717b4a847a6aa07c58a7605
Joker <8349854+jokerlay@user.noreply.gitee.com>
2024-04-09 23:18:13 +0800

    feat: 改金额

--- script.js
+++ script.js
@@ -365,7 +365,7 @@ is_user_logged_in()
           total: {
             label: "My Demo Company",
             type: "final",
-            amount: "0.01",
+            amount: "2",
           }
         };
         current_ap_session = new ApplePaySession(4, payment_request);

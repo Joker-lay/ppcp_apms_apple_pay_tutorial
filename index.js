dea293e794018e8055e9a34705af35319f3c7d62
Joker <8349854+jokerlay@user.noreply.gitee.com>
2024-04-13 18:03:53 +0800

    feat: 更新价格

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/create_order', (req, res) => {
                 'purchase_units': [{
                     'amount': {
                         'currency_code': 'USD',
-                        'value': '0.01'
+                        'value': '3.24'
                     }
                 }]
             };
